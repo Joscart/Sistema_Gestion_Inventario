5063308b48c62c88cf420a41a80794e6e20fb386
Jose <jguallasaminc@est.ups.edu.ec>
2024-05-24 18:29:14 -0500

    Correccion errores

--- src/modelo/ValidarDatosCliente.java
+++ src/modelo/ValidarDatosCliente.java
@@ -19,16 +19,25 @@ public class ValidarDatosCliente {
 	        return direccion.matches(direccion);
 	    }
 	 
-	    public static boolean validarDetallesDeContacto(String contacto) {
-	        String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,63}$";
+	    public static boolean validarDNI(String dni) {
+			String regex = "^[0-2][0-9]{8}[-]?[0-9]$";
 	        Pattern pattern = Pattern.compile(regex);
-	        Matcher matcher = pattern.matcher(contacto);
-	        return contacto.matches(contacto);
+	        Matcher matcher = pattern.matcher(dni);
+	        return matcher.matches();
+		} 	 
+	    
+	    public static boolean ValidarEmail(String email) {
+	        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+	        Pattern pattern = Pattern.compile(emailRegex);
+	        return pattern.matcher(email).matches();
 	    }
-	 
+
+	    public static boolean ValidarTelefono(String phone) {
+	        String phoneRegex = "^(\\+\\d{1,3}[- ]?)?\\d{10}$";
+	        Pattern pattern = Pattern.compile(phoneRegex);
+	        return pattern.matcher(phone).matches();
+	    }
+	    
+	    
 }
-
-
-
-
 
